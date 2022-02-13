ddd0ebaaf981e10bdec11f88ad017f8cdb9c8933
name-666 <57955820+name-666@users.noreply.github.com>
2022-02-13 22:12:14 +0200

    Что-то сделал. Не знаю, верно или нет
    Наверное, правила хорошего тона гласит в необходимости писать изменения. 
    1- Добавлен генератор псевдослучайных чисел. 
    2- Добавлены константы с минимальном и максимальном значением. 
    3- Добавлена проверка имени на пустую строку.

--- CleanetCode.TypesBranchesAndLoops/CleanetCode.GuessingNumberGame/Program.cs
+++ CleanetCode.TypesBranchesAndLoops/CleanetCode.GuessingNumberGame/Program.cs
@@ -1,47 +1,54 @@
-// See https://aka.ms/new-console-template for more information
-using System.Text;
+const int minNumber = 0;  // так называемые магичиские цифры.
+            const int maxNumber = 10; // так называемые магичиские цифры.
 
-Console.OutputEncoding = Encoding.UTF8;
+            
 
-Console.WriteLine("Добро пожаловать в игру 'Угадай число'");
-Console.WriteLine("Как вас зовут?");
-string? userName = Console.ReadLine();
-Console.WriteLine(
-	$"Привет {userName}. " +
-	$"Я загадал тебе число от 0 до 99. " +
-	$"Попробуй отгадать");
+            Console.WriteLine("Добро пожаловать в игру 'Угадай число'");
+            Console.WriteLine("Как вас зовут?");
+            string? userName = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(userName))  // если имя окажеться пустым или будет содержать пробел, то порисвоется значение по "умолчанию" 
+            {                                         // если имя окажеться пустым или будет содержать пробел, то порисвоется значение по "умолчанию" 
+                userName = "Unknow";                  // если имя окажеться пустым или будет содержать пробел, то порисвоется значение по "умолчанию" 
+            }                                         
+            Console.WriteLine(
+                $"Привет {userName}. " +
+                $"Я загадал тебе число от {minNumber} до {maxNumber}. " +
+                $"Попробуй отгадать");
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(minNumber, maxNumber);
+            bool isWin = false;
+            int count=1; // число ходов. Минимальное число ходов 1. Так как с первого раза угаданно число. Это минимальное число.
 
-int secretNumber = 42;
-bool isWin = false;
+            int userNumber;           // нет необходимости в цыкле проверять каждый раз.
+            bool isIntNumber = false; // нет необходимости в цыкле проверять каждый раз.
+            while (!isWin)
+            {
+                do
+                {
+                    Console.WriteLine($"Введи число от {minNumber} до  {maxNumber}");
+                    string? userInput = Console.ReadLine();
+                    isIntNumber = int.TryParse(userInput, out userNumber);
 
-while (!isWin)
-{
-	int userNumber = -1;
-	bool isIntNumber = false;
-	do
-	{
-		Console.WriteLine("Введи число от 0 до 99");
-		string? userInput = Console.ReadLine();
-		isIntNumber = int.TryParse(userInput, out userNumber);
+                    if (!isIntNumber)
+                    {
+                        Console.WriteLine($"Вы ввели {userInput}. Нужно ввести число от {minNumber}  до  {maxNumber}");
+                    }
+                }
+                while (!isIntNumber);
 
-		if (!isIntNumber)
-		{
-			Console.WriteLine($"Вы ввели {userInput}. Нужно ввести число от 0 до 99");
-		}
-	} while (!isIntNumber);
-
-	if (userNumber > secretNumber)
-	{
-		Console.WriteLine($"Ваше число ({userNumber}) больше, чем загаданное");
-	}
-	else if (userNumber < secretNumber)
-	{
-		Console.WriteLine($"Ваше число ({userNumber}) меньше, чем загаданное");
-	}
-	else
-	{
-		isWin = true;
-		Console.WriteLine("Вы победили!");
-	}
-}
-
+                if (userNumber > secretNumber)
+                {
+                    count++;
+                    Console.WriteLine($"Ваше число ({userNumber}) больше, чем загаданное");
+                }
+                else if (userNumber < secretNumber)
+                {
+                    count++;
+                    Console.WriteLine($"Ваше число ({userNumber}) меньше, чем загаданное");
+                }
+                else
+                {
+                    isWin = true;
+                    Console.WriteLine($"{userName}, вы победили!   Затратив {count} ходов");
+                }
+            }
